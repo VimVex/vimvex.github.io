34649b2eca6d6eb71877da0e5837f4c80f082967
VimVex <57888165+VimVex@users.noreply.github.com>
2023-05-15 20:26:53 +0400

    Update main.js

--- TestProject3/js/main.js
+++ TestProject3/js/main.js
@@ -1,4 +1,12 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.nav');
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+}
 
+// scroll section
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
@@ -16,4 +24,7 @@ window.onscroll = () => {
 	});
 	let header = document.querySelector('header');
 	header.classList.toggle('sticky', window.scrollY > 100);
+
+	menuIcon.classList.remove('bx-x');
+	navbar.classList.remove('active');
 }
