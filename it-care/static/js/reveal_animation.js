6ea8a8ca983c4f2ba152c30d7b7b95ba66300653
VimVex <57888165+VimVex@users.noreply.github.com>
2023-07-31 09:41:10 +0400

    Add files via upload

--- it-care/static/js/reveal_animation.js
+++ it-care/static/js/reveal_animation.js
@@ -1,19 +1,40 @@
 window.addEventListener('scroll', reveal);
 
 function reveal(){
-	var reveals = document.querySelectorAll('.reveal');
+ 	var reveals = document.querySelectorAll('.reveal');
 
-	for(var i = 0; i < reveals.length; i++){
+ 	for(var i = 0; i < reveals.length; i++){
 
-		var windowheight = window.innerHeight;
-		var revealtop = reveals[i].getBoundingClientRect().top;
-		var revealpoint = 150;
+ 		var windowheight = window.innerHeight;
+ 		var revealtop = reveals[i].getBoundingClientRect().top;
+ 		var revealpoint = 150;
 
-		if(revealtop < windowheight - revealpoint){
-			reveals[i].classList.add('active');
-		}
-		else{
+ 		if(revealtop < windowheight - revealpoint){
+ 			reveals[i].classList.add('active');
+ 		}
+ 		else{
 			reveals[i].classList.remove('active');
-		}
-	}
-}
+ 		}
+ 	}
+ }
+
+
+// const animation_elements = document.querySelectorAll('.reveal');
+
+// const observer = new IntersectionObserver((entries) => {
+// 	entries.forEach((entry) => {
+// 		if (entry.isIntersecting) {
+// 			entry.target.classList.add('.scrollanimate');
+// 		} else {
+// 			entry.target.classList.remove('.scrollanimate');
+// 		}
+// 	})
+// }, {
+// 	threshold: 0.5
+// });
+
+// for (let i = 0; i < animation_elements.length; i++) {
+// 	const el = animation_elements[i];
+
+// 	observer.observe(el);
+// }
